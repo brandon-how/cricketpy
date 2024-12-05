af1ba8380526f57447db7ac0acf0b2dd71e21e8c
brandonhao25@gmail.com <brandonhao25@gmail.com>
2024-12-05 22:18:36 +1100

    Add user-agent header to bypass bot assumption

--- fetch_cricket_info.py
+++ fetch_cricket_info.py
@@ -14,10 +14,15 @@ load_dotenv(env_path)
 env = os.getenv("env")
 
 # %%
+
 url = "https://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;page=1;size=200;template=results;type=batting;wrappertype=print"
-pd.read_html(url)
-# page = requests.get(url)pip
-# soup = BeautifulSoup(page.content, "html.parser")
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, "html.parser")
 
 
 # %%
