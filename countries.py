529e2c69a72485a2f1baa2f719b02596cfad1df7
bhao <brandon.hao@herdml.org>
2024-12-06 11:43:06 +1100

    Add rename_country function

--- countries.py
+++ countries.py
@@ -1,3 +1,5 @@
+import re
+
 men = {
     1: "England",
     2: "Australia",
@@ -44,3 +46,58 @@ women = {
     3505: "Scotland",
     3843: "Trinidad & Tobago",
 }
+
+
+def rename_countries(country: str) -> str:
+    """
+    Standardizes and replaces country abbreviations or shorthand with their full names.
+
+    Args:
+        country (str): A country name or abbreviation (e.g., "AFG", "AUS", "ENG") to be renamed.
+
+    Returns:
+        str: The standardized country name (e.g., "Afghanistan", "Australia", "England").
+    """
+    # Define a series of patterns and their replacements
+    patterns = [
+        (r"AFG", "Afghanistan"),
+        (r"Afr$", "Africa XI"),
+        (r"AUS", "Australia"),
+        (r"Bdesh|BDESH|BD", "Bangladesh"),
+        (r"BMUDA", "Bermuda"),
+        (r"CAN", "Canada"),
+        (r"DnWmn|Denmk", "Denmark"),
+        (r"EAf", "East (and Central) Africa"),
+        (r"ENG", "England"),
+        (r"HKG", "Hong Kong"),
+        (r"ICC$", "ICC World XI"),
+        (r"INDIA|IND", "India"),
+        (r"IntWn|Int XI", "International XI"),
+        (r"Ire$|IRELAND|IRE", "Ireland"),
+        (r"JamWn", "Jamaica"),
+        (r"JPN", "Japan"),
+        (r"KENYA", "Kenya"),
+        (r"NAM", "Namibia"),
+        (r"NEPAL", "Nepal"),
+        (r"Neth$|NL", "Netherlands"),
+        (r"NZ", "New Zealand"),
+        (r"OMAN", "Oman"),
+        (r"PAK", "Pakistan"),
+        (r"PNG|P\\.N\\.G\\.", "Papua New Guinea"),
+        (r"^SA", "South Africa"),
+        (r"SCOT|SCO|Scot$", "Scotland"),
+        (r"SL", "Sri Lanka"),
+        (r"TTWmn|T & T", "Trinidad and Tobago"),
+        (r"UAE|U\\.A\\.E\\.", "United Arab Emirates"),
+        (r"USA|U\\.S\\.A\\.", "United States of America"),
+        (r"World$|World-XI", "World XI"),
+        (r"WI", "West Indies"),
+        (r"YEWmn|Y\\. Eng", "Young England"),
+        (r"ZIM", "Zimbabwe"),
+    ]
+
+    # Search through each pattern and replacement
+    for pattern, replacement in patterns:
+        country = re.sub(pattern, replacement, country)
+
+    return country
