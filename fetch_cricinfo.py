dd17b440cff4c157eb29dd618b9f39be12e119d7
brandon.hao@herdml.org <brandon.hao@herdml.org>
2024-12-23 11:33:09 +1100

    clean: :wastebasket: removes redundant code

--- fetch_cricinfo.py
+++ fetch_cricinfo.py
@@ -2,18 +2,12 @@
 import requests
 import pandas as pd
 import numpy as np
-import os
-from pathlib import Path
-from dotenv import load_dotenv
 from countries import *
 from difflib import get_close_matches
 from io import StringIO
-from typing import Tuple, Literal, Optional
+from typing import Literal, Optional
 
-# Set environment
-env_path = Path(__file__).parent / ".env"
-load_dotenv(env_path)
-env = os.getenv("env")
+# %%
 
 
 # %%
